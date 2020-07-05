3727e4019c0db470fdb9c155280c85206a9b97e7
Diogo Serra Duque <diogo.serra.duque@blip.pt>
2020-07-05 23:26:14 +0100

    Updates

---
+++ precache-manifest.b09f6e435c34bde019a04817b5ef8855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7426bd436fdde19160ff8882450c12ab",
+    "url": "/covid19TrofaGraph/index.html"
+  },
+  {
+    "revision": "c7dd90d2169c46252315",
+    "url": "/covid19TrofaGraph/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bfd0e9476d06bcea70ae",
+    "url": "/covid19TrofaGraph/static/js/2.33191585.chunk.js"
+  },
+  {
+    "revision": "9d6ab11c62d01ea9eaac58487c5f0b6b",
+    "url": "/covid19TrofaGraph/static/js/2.33191585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7dd90d2169c46252315",
+    "url": "/covid19TrofaGraph/static/js/main.fc5716d6.chunk.js"
+  },
+  {
+    "revision": "5f308162fecbf0d8eaf6",
+    "url": "/covid19TrofaGraph/static/js/runtime-main.fadebc51.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19TrofaGraph/precache-manifest.d7008984818c9245e25343885051ec69.js"
+  "/covid19TrofaGraph/precache-manifest.b09f6e435c34bde019a04817b5ef8855.js"
 );
 
 self.addEventListener('message', (event) => {
