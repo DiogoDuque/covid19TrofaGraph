6c5719059308ea4f080e02c11418eb97e2a0e305
Diogo Serra Duque <diogo.serra.duque@blip.pt>
2020-12-08 01:04:15 +0000

    theme refactoring

--- src/config/themes.js
+++ src/config/themes.js
@@ -1,40 +1,19 @@
-export const themeCyan = {
-  backgroundColor: 'rgba(75,192,192,0.4)',
-  borderColor: 'rgba(75,192,192,1)',
-  pointBorderColor: 'rgba(75,192,192,1)',
-  pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+function getThemeFromRGB(r,g,b) {
+  const unified = `${r},${g},${b}`;
+  return {
+    backgroundColor: `rgba(${unified},0.4)`,
+    borderColor: `rgba(${unified},1)`,
+    pointBorderColor: `rgba(${unified},1)`,
+    pointHoverBackgroundColor: `rgba(${unified},1)`,
+  }
 }
 
-export const themeMagenta = {
-  backgroundColor: 'rgba(192,75,192,0.4)',
-  borderColor: 'rgba(192,75,192,1)',
-  pointBorderColor: 'rgba(192,75,192,1)',
-  pointHoverBackgroundColor: 'rgba(192,75,192,1)',
-}
+export const themeCyan = getThemeFromRGB(75, 192, 192);
 
-export const themeMagentaLight = {
-  backgroundColor: 'rgba(217, 128, 217,0.4)',
-  borderColor: 'rgba(217, 128, 217,1)',
-  pointBorderColor: 'rgba(217, 128, 217,1)',
-  pointHoverBackgroundColor: 'rgba(217, 128, 217,1)',
-}
+export const themeMagenta = getThemeFromRGB(192, 75, 192);
 
-export const themeMagentaDark = {
-  backgroundColor: 'rgba(140, 83, 140,0.4)',
-  borderColor: 'rgba(140, 83, 140,1)',
-  pointBorderColor: 'rgba(1140, 83, 140,1)',
-  pointHoverBackgroundColor: 'rgba(140, 83, 140,1)',
-}
+export const themeMagentaLight = getThemeFromRGB(217, 128, 217);
 
-export const themeYellow = {
-  backgroundColor: 'rgba(192,192,75,0.4)',
-  borderColor: 'rgba(192,192,75,1)',
-  pointBorderColor: 'rgba(192,192,75,1)',
-  pointHoverBackgroundColor: 'rgba(192,192,75,1)',
-}
+export const themeMagentaDark = getThemeFromRGB(140, 83, 140);
 
-export default {
-  themeCyan,
-  themeMagenta,
-  themeYellow,
-}
+export const themeYellow = getThemeFromRGB(192, 192, 75);
