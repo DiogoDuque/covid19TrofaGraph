0f1267e4ff94160bcc2d70e7b714ea23b7abb26d
DiogoDuque <diogo.serra.duque@blip.pt>
2020-12-26 00:33:17 +0000

    adds new themes

--- src/config/themes.js
+++ src/config/themes.js
@@ -9,20 +9,38 @@ function getThemeFromRGB(r,g,b, hasBackground=true, otherOptions={}) {
   }
 }
 
+// CYAN
 export const themeCyan = getThemeFromRGB(75, 192, 192);
+export const themeCyanNoBG = getThemeFromRGB(75, 192, 192, false);
 
+
+// MAGENTA
 export const themeMagenta = getThemeFromRGB(192, 75, 192);
-
+export const themeMagentaNoBG = getThemeFromRGB(192, 75, 192, false);
 export const themeMagentaLight = getThemeFromRGB(217, 128, 217);
-
 export const themeMagentaDark = getThemeFromRGB(140, 83, 140);
 
-export const themeYellow = getThemeFromRGB(192, 192, 75);
 
+// YELLOW
+export const themeYellow = getThemeFromRGB(192, 192, 75);
+export const themeYellowNoBG = getThemeFromRGB(192, 192, 75, false);
+export const themeYellowLight = getThemeFromRGB(217, 217, 128);
+export const themeYellowDark = getThemeFromRGB(140, 140, 83);
+
+
+// BLUE
+export const themeBlue = getThemeFromRGB(0, 80, 255);
+export const themeBlueNoBG = getThemeFromRGB(0, 80, 255, false);
+
+
+// GREEN
+export const themeGreen = getThemeFromRGB(0, 255, 80);
+export const themeGreenNoBG = getThemeFromRGB(0, 255, 80, false);
+
+
+// OTHERS
 export const themeGreyTransparent = getThemeFromRGB(200, 200, 200, false, {pointRadius:0, pointHitRadius:0, borderDash:[8]});
 
 export const severityTheme1 = getThemeFromRGB(255, 189, 0, false);
-
 export const severityTheme2 = getThemeFromRGB(255, 84, 0, false);
-
 export const severityTheme3 = getThemeFromRGB(158, 0, 89, false);
