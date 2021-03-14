7ea796abc272b2c2cf28f6887b2b5d6191bbec80
DuqueD <diogo.serra.duque@paddypowerbetfair.com>
2021-03-14 19:25:35 +0000

    fetching mechanics refactoring

--- src/utils/fetchData.tsx
+++ src/utils/fetchData.tsx
@@ -3,62 +3,51 @@ import Csv from 'csv-parser';
 import DateEntry from '../model/DateEntry';
 import { EntriesAggregatorBuilder, PtDataEntriesAggregatorBuilder, KEY } from '../model/EntriesAggregator';
 
-function _getDataFromSource(sourceFile: string): Promise<Response> {
-  const url = 'https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/' + sourceFile;
-  const headers: Headers = new Headers({
-    //'Content-Type': 'application/x-www-form-urlencoded',
-    //'Origin': 'https://github.com',
-    //'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    Accept: 'application/vnd.github.v3+json',
-    // 'Access-Control-Allow-Origin': '*',
-  });
-  console.log('[_getDataFromSource] Fetching ' + sourceFile);
-  return fetch(url, { method: 'GET', headers });
+function makeRequest(filename: string): Promise<Response> {
+  const url = 'https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/' + filename;
+    const headers: Headers = new Headers({
+      Accept: 'application/vnd.github.v3+json',
+    });
+    console.log('[getDataFromSource] Fetching ' + filename);
+    return fetch(url, { method: 'GET', headers })
+}
+
+function getDataFromSource(builder: EntriesAggregatorBuilder<string, DateEntry>,
+    callback: Function, filterFunc: Function = ()=>true) {
+  makeRequest(builder.name)
+  .then((response: Response) => {
+    if (response && response.status === 200)
+      return response.text();
+  })
+  .then(responseData => {
+    const stream = new Readable();
+    stream.push(responseData);
+    stream.push(null);
+    stream
+      .pipe(Csv())
+      .on('data', data =>
+        filterFunc(data)
+        && Object.values(KEY)
+        .forEach(key => data[key] && builder.addEntry(key, new DateEntry(data.data, data[key])))
+      )
+      .on('end', () => callback(builder.build()));
+  })
+  .catch(err => console.error(err));;
 }
 
 export function getTownData(town: string, callback: Function) {
   const filename = 'data_concelhos_new.csv';
-  _getDataFromSource(filename)
-    .then((response: Response) => {
-      if (response && response.status === 200)
-        return response.text();
-    })
-    .then(responseData => {
-      const builder = new EntriesAggregatorBuilder(filename);
-      const stream = new Readable();
-      stream.push(responseData);
-      stream.push(null);
-      stream
-        .pipe(Csv())
-        .on('data', data =>
-          data.concelho === town
-          && Object.values(KEY)
-          .forEach(key => data[key] && builder.addEntry(key, new DateEntry(data.data, data[key])))
-        )
-        .on('end', () => callback(builder.build()));
-    })
-    .catch(err => console.error(err));
+  getDataFromSource(
+    new EntriesAggregatorBuilder(filename),
+    callback,
+    (data: any) => data.concelho === town
+  );
 }
 
 export function getPortugalData(callback: Function) {
   const filename = 'data.csv';
-  _getDataFromSource(filename)
-    .then((response: Response) => {
-      if (response && response.status === 200)
-        return response.text();
-    })
-    .then(responseData => {
-      const builder = new PtDataEntriesAggregatorBuilder(filename);
-      const stream = new Readable();
-      stream.push(responseData);
-      stream.push(null);
-      stream
-        .pipe(Csv())
-        .on('data', data => {
-          Object.values(KEY)
-          .forEach(key => data[key] && builder.addEntry(key, new DateEntry(data.data, data[key])))
-        })
-        .on('end', () => callback(builder.build()));
-    })
-    .catch(err => console.error(err));
+  getDataFromSource(
+    new PtDataEntriesAggregatorBuilder(filename),
+    callback
+  );
 }
