99b80b7c32df827b1ca435b1c7d1c16a1cd45ddf
DuqueD <diogo.serra.duque@paddypowerbetfair.com>
2021-04-06 00:07:45 +0100

    adds elevated risk line to town chart

--- src/component/app/TownCharts.js
+++ src/component/app/TownCharts.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { chartGroupWrapper } from "../../utils/chartUtils";
 import MultiLineChart from "../chart/MultiLineChart";
-import { themeYellow, themeBlueTransparent } from "../../config/themes";
+import { themeYellow, themeBlueTransparent, severityTheme1 } from "../../config/themes";
 import { KEY } from "../../model/EntriesAggregator";
 import EntriesStore from "../../store/EntriesStore";
 import GeneralStore from "../../store/GeneralStore";
-import { convertDailyIncidencyToBiweeklyIncidency } from "../../utils/EntriesOps";
+import { convertDailyIncidencyToBiweeklyIncidency, getEntriesLineGenerator } from "../../utils/EntriesOps";
 
 const TownCharts = () => {
   const styles = GeneralStore.useState(s => s.styles);
@@ -19,6 +19,7 @@ const TownCharts = () => {
     ptEntries.getAll(KEY.INCIDENCE_PT),
     townEntriesIncidence14.map(e => e.x)
   );
+  const townLineGenerator = getEntriesLineGenerator(townEntriesIncidence14);
   const town = currTown[0] + currTown.slice(1).toLowerCase();
 
   return chartGroupWrapper(`Evolução - ${town}`, styles,
@@ -27,13 +28,15 @@ const TownCharts = () => {
       dataArray={[
         townEntriesIncidence14,
         ptEntriesIncidence14,
+        townLineGenerator(120*14),
       ]}
       dateRange={dateRange}
       labels={[
         `Incidência - ${town} (a 14 dias p/ 100k hab.)`,
         'Incidência em Portugal (a 14 dias p/ 100k hab.)',
+        'Risco elevado',
       ]}
-      themes={[themeYellow, themeBlueTransparent]}
+      themes={[themeYellow, themeBlueTransparent, severityTheme1]}
     />,
   );
 }
